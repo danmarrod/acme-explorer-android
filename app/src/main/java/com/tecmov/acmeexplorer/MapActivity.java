f63a14c4038d0856a836bcda3a078c7ca79e97ad
danmarrod <danmarrod@alum.us.es>
2021-07-15 21:56:09 +0200

    Added map activity and gmaps configuration

---
+++ app/src/main/java/com/tecmov/acmeexplorer/MapActivity.java
@@ -0,0 +1,37 @@
+package com.tecmov.acmeexplorer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        LatLng location = new LatLng(37.123, -6.541);
+        googleMap.addMarker(new MarkerOptions().position(location).title("Universidad de Sevilla"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+
+    }
+
+}
